8b92117200e122bbbd147d14aff3a0bd7070ad7d
A. Matías Quezada <amatiasq@gmail.com>
2017-11-17 17:00:21 +0100

    Fix: read is not working on install-mac/init.sh

--- install-mac/init.sh
+++ install-mac/init.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # This will request XCode command line tools installation
+
 git --version > /dev/null
 
 curl https://raw.githubusercontent.com/amatiasq/bin/master/run-once/generate-rsa.sh | bash -e
 
 echo 'Add key to Github, press [Enter] when done.'
 open 'https://github.com/settings/ssh'
-read
+read < /dev/tty
 
 git clone git@github.com:amatiasq/bin $HOME/bin
 
