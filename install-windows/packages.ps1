4c2a1a6fe7fbc6cd44129c5831b2880b81d233cd
A. Matías Quezada <amatiasq@gmail.com>
2019-02-13 16:16:09 +0100

    Create packages.ps1

---
+++ install-windows/packages.ps1
@@ -0,0 +1,21 @@
+# Then in another terminal execute this:
+choco install googlechrome -y
+choco install firefox -y
+choco install spotify -y
+choco install slack -y
+choco install steam -y
+
+# choco install clipclip -y -- not part of chocolatey packages
+choco install greenshot -y
+choco install nodejs -y
+choco install docker-desktop -y
+
+choco install vscode -y
+choco install sublimetext3 -y
+choco install notepadplusplus -y
+choco install gitkraken -y
+choco install visualstudio2017community -y
+
+choco install octave -y
+choco install blender -y
+choco install magicavoxelviewer -y
