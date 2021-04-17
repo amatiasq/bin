54a02c8e93021143d09fa502c36bed2c2348aa6b
A. Matías Quezada <amatiasq@gmail.com>
2021-04-17 12:55:20 +0200

    Fresh start - applications.sh

--- install/mac/applications.sh
+++ install/mac/applications.sh
@@ -8,10 +8,10 @@ brew cask install sublime-text
 # Development
 brew cask install visual-studio-code
 brew cask install teamviewer
-brew cask install skype
+#brew cask install skype
 brew cask install slack
-brew cask install parallels
-brew cask install poedit
+#brew cask install parallels
+#brew cask install poedit
 
 # Entretainment
 brew cask install spotify
@@ -23,17 +23,17 @@ brew cask install minecraft
 brew cask install alfred
 brew cask install keka
 brew cask install xscope
-brew cask install docker
+#brew cask install docker
 
 # foo
 brew cask install blender
 brew cask install brave-browser
 brew cask install calibre
 brew cask install discord
-brew cask install postman
+#brew cask install postman
 brew cask install processing
 brew cask install virtualbox
-brew cask install visual-studio
+#brew cask install visual-studio
 brew cask install calibre
 
 # cacher
