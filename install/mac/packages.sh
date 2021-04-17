8baeadcc934a26750d77acd42fefac3fedca23b5
A. Matías Quezada <amatiasq@gmail.com>
2021-04-17 12:47:00 +0200

    Fresh start - packages.sh

--- install/mac/packages.sh
+++ install/mac/packages.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Homebrew cask
-brew tap caskroom/cask
+brew tap homebrew/cask
 
 # Update homebrew
 brew update
@@ -14,13 +14,13 @@ brew install deno
 brew install python3
 brew install zsh
 brew install wget
-brew install hub
+#brew install hub
 brew install jq
 brew install watch
-brew install docker
-brew install imagemagick
-brew install youtube-dl
-brew install exiftool
+#brew install docker
+4brew install imagemagick
+#brew install youtube-dl
+#brew install exiftool
 brew install asciinema
 
 # NVM is a special case
