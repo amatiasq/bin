8fc4bb9ace6d3963d5def1cf9bcd0c501f13f1d4
A. Matías Quezada <amatiasq@gmail.com>
2022-10-07 17:40:49 +0200

    Fixes on NVM

--- install/mac/node.sh
+++ install/mac/node.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+source $(brew --prefix nvm)/nvm.sh
+
 nvm install stable
-nvm install lts/*
+nvm install 18
+nvm install 16
+nvm install 14
+nvm install 12
+
 nvm use stable
 
 npm install -g eslint
