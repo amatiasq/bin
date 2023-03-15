1ad98bb33523b882f4461565c8f98202a7722ecf
A. Matías Quezada <amatiasq@gmail.com>
2023-03-15 02:50:47 +0100

    Add rust to mac installation files

---
+++ install/mac/rust.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source "$HOME/.cargo/env"
+
+rustup target add wasm32-unknown-unknown
+
+cargo install --locked trunk
+cargo install cargo-generate
