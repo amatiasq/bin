d6577a9b6760084e5ef5b9efce34fa51ee4b0a9b
A. Matías Quezada <amatiasq@gmail.com>
2023-03-31 11:32:09 +0200

    Add cargo-edit and cargo-watch

--- install/mac/rust.sh
+++ install/mac/rust.sh
@@ -7,4 +7,8 @@ source "$HOME/.cargo/env"
 rustup target add wasm32-unknown-unknown
 
 cargo install --locked trunk
+cargo install cargo-edit
+cargo install cargo-watch
 cargo install cargo-generate
+
+# cargo install cargo-binstall
